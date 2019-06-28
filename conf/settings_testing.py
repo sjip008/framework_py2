0404c4047e3c4bfda02c804943b8f3b56e7d8450
hsq <314629925>
2019-06-28 17:45:06 +0800

    df

--- conf/settings_testing.py
+++ conf/settings_testing.py
@@ -19,11 +19,11 @@ from settings import APP_ID
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',  # 默认用mysql
-        'NAME': APP_ID,                        # 数据库名 (默认与APP_ID相同)
-        'USER': '',                            # 你的数据库user
-        'PASSWORD': '',                        # 你的数据库password
-        'HOST': '',                   		   # 数据库HOST
-        'PORT': '',                        # 默认3306
+        'NAME': 'hanshiqindb2',                        # 数据库名 (默认与APP_ID相同)
+        'USER': 'root',                            # 你的数据库user
+        'PASSWORD': 'Uqv.83WuNm',                        # 你的数据库password
+        'HOST': '10.0.1.192',                   		   # 数据库HOST
+        'PORT': '3306',                        # 默认3306
     },
 }
 
